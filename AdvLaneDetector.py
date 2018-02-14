edabc33b8a5058a2ae776ab0bdf621924d1f2de5
Grigorii <grygoryant@gmail.com>
2018-02-15 02:25:58 +0300

    Calculating lane curvature

--- AdvLaneDetector.py
+++ AdvLaneDetector.py
@@ -1,4 +1,3 @@
-from Line import Line
 import numpy as np
 import cv2
 import os
@@ -29,8 +28,6 @@ def calc_reproj_error(obj_points, img_points, rvecs, tvecs, mtx, dist):
 class AdvLaneDetector:
 
 	def __init__(self):
-		self.left_line = Line()
-		self.right_line = Line()
 		self.camera = CameraParams()
 		self.img_preproc = ImagePreprocessor()
 		self.sw_scanner = SlidingWindowScanner()
@@ -93,13 +90,20 @@ class AdvLaneDetector:
 
 		preproc_img = self.img_preproc.process_image(undist)
 
-		[windows_layer, lines_layer, points_layer, lane_layer] = self.sw_scanner.process_image(preproc_img)
+		[windows_layer, lines_layer, 
+			points_layer, lane_layer, 
+			left_rad, right_rad,
+			left_dist, right_dist, lane_width] = self.sw_scanner.process_image(preproc_img)
+
+		data_overlay = img.copy()
+		cv2.rectangle(data_overlay, (0,0), (img.shape[1], img.shape[0]//4), (0,0,0), -1)
 
 		lines_layer_transformed = perspective_transform(lines_layer, reverse=True)
 		lane_layer_transformed = perspective_transform(lane_layer, reverse=True)
 		img_bird_view = perspective_transform(img)
 
-		result = cv2.addWeighted(img, 1., lane_layer_transformed, .5, 0.)
+		result = cv2.addWeighted(img, 0.6, data_overlay, 0.4, 0.)
+		result = cv2.addWeighted(result, 1., lane_layer_transformed, .5, 0.)
 		result = cv2.addWeighted(result, 1., lines_layer_transformed, 1., 0.)
 
 		scanning_window = cv2.addWeighted(img_bird_view, 1., points_layer, .5, 0.)
@@ -107,12 +111,39 @@ class AdvLaneDetector:
 		scanning_window = cv2.addWeighted(scanning_window, 1., points_layer, 1., 0.)
 		scanning_window = cv2.addWeighted(scanning_window, 1., lines_layer, 1., 0.)
 
-		small_scanning_window = cv2.resize(scanning_window, (0,0), fx=0.4, fy=0.4)
+		small_scanning_window = cv2.resize(scanning_window, (0,0), fx=0.25, fy=0.25)
 
 		x_offset = img.shape[1] - small_scanning_window.shape[1]
 		y_offset = 0
 		result[y_offset:y_offset + small_scanning_window.shape[0], 
 			x_offset:x_offset + small_scanning_window.shape[1]] = small_scanning_window
+
+		cv2.putText(result, 'Radius of curvature: {:.2f} m'.format((left_rad+right_rad)/2), 
+			(img.shape[1]//10, img.shape[0]//10), 
+			cv2.FONT_HERSHEY_SIMPLEX, .8, (255, 255, 255), 2)
+
+		lane_center_x = lane_width/2
+		dist_from_center_1 = abs(lane_center_x - left_dist)
+		dist_from_center_2 = abs(lane_center_x - right_dist)
+		avg_dist_from_center = (dist_from_center_1 + dist_from_center_2)/2
+		dist_text = 'Distance from center: {:.2f} m to the '.format(avg_dist_from_center)
+		if left_dist > right_dist:
+			dist_text += 'right'
+		else:
+			dist_text += 'left'
+
+		cv2.putText(result, dist_text, 
+			(img.shape[1]//10, img.shape[0]//10 + 30), 
+			cv2.FONT_HERSHEY_SIMPLEX, .8, (255, 255, 255), 2)
+		cv2.putText(result, 'Left distance: {:.2f} m'.format(left_dist), 
+			(img.shape[1]//10, img.shape[0]//10 + 60), 
+			cv2.FONT_HERSHEY_SIMPLEX, .8, (255, 255, 255), 2)
+		cv2.putText(result, 'Right distance: {:.2f} m'.format(right_dist), 
+			(img.shape[1]//10, img.shape[0]//10 + 90), 
+			cv2.FONT_HERSHEY_SIMPLEX, .8, (255, 255, 255), 2)
+		#cv2.putText(result, 'Lane width: {:.2f} m'.format(lane_width), 
+		#	(img.shape[1]//10, img.shape[0]//10 + 90), 
+		#	cv2.FONT_HERSHEY_SIMPLEX, .8, (255, 255, 255), 2)
 
 		return result
 
@@ -136,8 +167,8 @@ def process_image(image):
 
     return res
 
-output = './challenge_video_annotated.mp4'
-clip1 = VideoFileClip("./challenge_video.mp4")#.subclip(41.5,43)
+output = './project_video_annotated1.mp4'
+clip1 = VideoFileClip("./project_video.mp4")#.subclip(5,6)
 white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
 white_clip.write_videofile(output, audio=False)
 
--- SlidingWindowScanner.py
+++ SlidingWindowScanner.py
@@ -7,6 +7,8 @@ from collections import deque
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 
+m_per_pix_y = 30 / 720
+m_per_pix_x = 3.7 / 700
 
 def get_n_dist_maxs(array, dist, n):
 	avg_val = sum(array)/len(array)
@@ -27,6 +29,21 @@ def get_n_dist_maxs(array, dist, n):
 				if distance > dist:
 					maxs.append(m)
 		return np.flip(sorted(maxs[:n], key=lambda x:x[0]), axis=0).astype(int)
+
+def calc_curv_rad(fit_coords, y_val):
+	y = fit_coords[:, 1]
+	x = fit_coords[:, 0]
+	fit = np.polyfit(y * m_per_pix_y, x * m_per_pix_x, 2)
+	return ((1 + (2*fit[0]*y_val*m_per_pix_y + fit[1])**2)**1.5) / np.absolute(2*fit[0])
+
+def calc_lane_width(fit_left_coords, fit_right_coords, img_width, img_height):
+	x_left = fit_left_coords[img_height-1][0]
+	x_right = fit_right_coords[img_height-1][0]
+	return np.absolute((x_right - x_left) * m_per_pix_x)
+
+def calc_dist_to_line(fit_coords, img_width, img_height):
+	x = fit_coords[img_height-1][0]
+	return np.absolute((img_width//2 - x) * m_per_pix_x)
 
 class SlidingWindowScanner:
 
@@ -86,6 +103,9 @@ class SlidingWindowScanner:
 		points_layer = np.dstack((img, img, img))*0
 		lane_layer = np.dstack((img, img, img))*0
 
+		#plt.imshow(img)
+		#lt.show()
+
 		if len(lane_peaks) > 1:
 			left_wind_init_idx = lane_peaks[0][0]
 			right_wind_init_idx = lane_peaks[1][0]
@@ -135,6 +155,12 @@ class SlidingWindowScanner:
 			left_fit_coords = self.get_curve_coords(img.shape[0], left_fit)
 			right_fit_coords = self.get_curve_coords(img.shape[0], right_fit)
 
+			left_curv_rad = calc_curv_rad(left_fit_coords, img.shape[0]-1)
+			right_curv_rad = calc_curv_rad(right_fit_coords, img.shape[0]-1)
+			left_dist = calc_dist_to_line(left_fit_coords, img.shape[1], img.shape[0])
+			right_dist = calc_dist_to_line(right_fit_coords, img.shape[1], img.shape[0])
+			lane_width = calc_lane_width(left_fit_coords, right_fit_coords, img.shape[1], img.shape[0])
+
 			points_layer[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
 			points_layer[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
 
@@ -145,7 +171,10 @@ class SlidingWindowScanner:
 
 			self.windows_initialized = True
 
-		return [windows_layer, lines_layer, points_layer, lane_layer]
+		return [windows_layer, lines_layer, 
+			points_layer, lane_layer, 
+			left_curv_rad, right_curv_rad,
+			left_dist, right_dist, lane_width]
 
 	def adjust_lanes(self, img):
 		nonzero = img.nonzero()
@@ -184,6 +213,12 @@ class SlidingWindowScanner:
 		left_fit_coords = self.get_curve_coords(img.shape[0], left_fit)
 		right_fit_coords = self.get_curve_coords(img.shape[0], right_fit)
 
+		left_curv_rad = calc_curv_rad(left_fit_coords, img.shape[0]-1)
+		right_curv_rad = calc_curv_rad(right_fit_coords, img.shape[0]-1)
+		left_dist = calc_dist_to_line(left_fit_coords, img.shape[1], img.shape[0])
+		right_dist = calc_dist_to_line(right_fit_coords, img.shape[1], img.shape[0])
+		lane_width = calc_lane_width(left_fit_coords, right_fit_coords, img.shape[1], img.shape[0])
+
 		self.draw_line_area(windows_layer, left_fit_coords)
 		self.draw_line_area(windows_layer, right_fit_coords)
 
@@ -195,7 +230,10 @@ class SlidingWindowScanner:
 
 		self.draw_lane(lane_layer, left_fit_coords, right_fit_coords)
 
-		return [windows_layer, lines_layer, points_layer, lane_layer]
+		return [windows_layer, lines_layer, 
+			points_layer, lane_layer, 
+			left_curv_rad, right_curv_rad,
+			left_dist, right_dist, lane_width]
 
 	def process_image(self, img):
 		if not self.windows_initialized:
